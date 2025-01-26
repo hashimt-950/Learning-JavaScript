c9ff1d13d59d5e2cdcccabfeb801629da018b7f0
Hashimt-950 <hashimt950@gmail.com>
2025-01-26 19:43:13 +0500

    added the code to get computers choice

--- rps.js
+++ rps.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+function getComputerChoice(){
+   let computer_choice
+   let random = Math.random()
+   if (random >= 0 && random <0.3) {
+      computer_choice = "rock"
+   }
+   else if (random >= 0.3 && random < 0.6) {
+      computer_choice = "paper"
+   }
+   else{
+      computer_choice = "scissors"
+   }
+   return computer_choice
+}
+
+console.log(getComputerChoice())
